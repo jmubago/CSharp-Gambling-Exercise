dbf1cb1c187137cb4deaa14d33d9ead126655756
jmubago <jmubagosotomayor@gmail.com>
2018-09-13 12:38:53 +0200

    Agregar archivos de proyecto.

---
+++ 02.3_GamblerProblem/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _02._3_GamblerProblem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.LstView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Location = new System.Drawing.Point(169, 276);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(190, 36);
+            this.BtnPlay.TabIndex = 0;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // LstView
+            // 
+            this.LstView.Location = new System.Drawing.Point(79, 34);
+            this.LstView.Name = "LstView";
+            this.LstView.Size = new System.Drawing.Size(323, 207);
+            this.LstView.TabIndex = 1;
+            this.LstView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 364);
+            this.Controls.Add(this.LstView);
+            this.Controls.Add(this.BtnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.ListView LstView;
+    }
+}
+
---
+++ 02.3_GamblerProblem/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02._3_GamblerProblem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //A gambler has a choice of two games. The first costs $8.00 to play. 
+        //Two dice are rolled and the player receive the sum
+        //of the numbers rolled in dollars.The second game costs $15.00 to play.
+        //Two dice are rolled and the player receives the product of the numbers rolled in dollars.
+
+        //Write a program which simulates each game being played 1000 times 
+        //and calculates the average winnings per game. Also determine which game the gambler should choose. 
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            LstView.Clear();
+
+            int game1 = 0;
+            int game2 = 0;
+            int averageGame1 = 0;
+            int averageGame2 = 0;
+
+            for(int i = 0; i < 1000; i++)
+            {
+                Random r = new Random();
+
+                //game 1
+                int dice1 = r.Next(1, 7);
+                int dice2 = r.Next(1, 7);
+                game1 += dice1 + dice2 - 8;
+
+                //game 2
+                int dice3 = r.Next(1, 7);
+                int dice4 = r.Next(1, 7);
+                game2 += dice3 * dice4 - 15;
+            }
+            averageGame1 = game1 / 1000;
+            averageGame2 = game2 / 1000;
+
+            LstView.Items.Add("Average Game1 = " + averageGame1.ToString());
+            LstView.Items.Add("Average Game2 = " + averageGame2.ToString());
+
+
+            if (averageGame1 <= 0 && averageGame2 <= 0)
+            {
+                LstView.Items.Add("Better stop gambling");
+            }
+            else if (averageGame1 > averageGame2)
+            {
+                LstView.Items.Add("Game 1 offers more profit");
+            }else if(averageGame1 < averageGame2)
+            {
+                LstView.Items.Add("Game 2 offers more profit");
+            }
+            else
+            {
+                LstView.Items.Add("Both games offer same profit");
+            }
+        }
+    }
+}
